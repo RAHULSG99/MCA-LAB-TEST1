19273a7ea1a0b43f55dee08a55714910fc3c0b27
RAHULSG99 <rahulsg40@gmail.com>
2022-11-22 20:27:46 +0530

    MCALABTEST1

---
+++ MCA LAB TEST 1/Objects/main.d
@@ -0,0 +1,3 @@
+.\objects\main.o: MCA LAB TEST 1\CODE-LED\main.c
+.\objects\main.o: MCA LAB TEST 1\CODE-LED\timer.h
+.\objects\main.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
---
+++ MCA LAB TEST 1/Objects/mainlcd.d
@@ -0,0 +1,3 @@
+.\objects\mainlcd.o: MCA LAB TEST 1\CODE-LCD\mainlcd.c
+.\objects\mainlcd.o: MCA LAB TEST 1\CODE-LCD\lcd.h
+.\objects\mainlcd.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
